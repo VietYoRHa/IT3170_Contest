1e984f777d5ec62b30c4978045fc77ee5134322b
VietYoRHa <thaiviet04@gmail.com>
2023-10-18 23:47:04 +0700

    all test cases passed

--- count_integer_linear_equation.cpp
+++ count_integer_linear_equation.cpp
@@ -5,13 +5,13 @@ vector<int> coefficients;
 int solutions = 0;
 
 void solutionCount(int n, int M, int index){
-    if (M == 0 && index == n){
+    if (M == 0 && index == n){ // M = 0 and checked final coefficient
         solutions++;
         return;
     }
-    if (M < 0 || index == n) return;
+    if (M < 0 || index == n) return; // M < 0 during/finish checking or checked final coefficient
     for (int x = 1; x <= M / coefficients[index]; x++){
-        solutionCount(n, M - x*coefficients[index], index + 1);
+        solutionCount(n, M - x*coefficients[index], index + 1); // else call solve a2x2 + a3x3 + ... + anxn = M - a1x1
     }
 }
 
@@ -25,7 +25,7 @@ int main(){
         cin >> input;
         coefficients.push_back(input);
     }
-    solutionCount(n, M, 0);
+    solutionCount(n, M, 0); // call solve a1x1 + a2x2 + ... + anxn = M
     cout << solutions << endl;
     return 0;
 }
